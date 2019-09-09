2bd807283b89d6b3c1fc48610d3f0163853457b6
Martin Moy <martinmoy1991@gmail.com>
2019-09-09 00:58:59 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,46 @@
 
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+  end
+end
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.length <=> b.length
+  end
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def sum_array(array)
+  array.inject {|sum, a| sum + a}
+end
+
+def swap_elements(array)
+  array[1],array[2]=array[2], array [1]
+  array
+end
+
+def kesha_maker(array)
+  array.map {|a| a[2]= "$"}
+  array
+end
+
+def find_a(array)
+  array.select { |a| a[0] == "a" }
+end
+
+def add_s(array)
+  array.map.with_index do |item, index|
+       index != 1 ? item << "s" : item
+  end
+end
